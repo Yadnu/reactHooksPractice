4a8a2862836a6b412f4fb8a8764cf81decc384f3
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-02-27 10:14:46 +0530

    Prop drilling practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,54 @@
-import { lazy, Suspense } from 'react'
+// import { lazy, Suspense } from 'react'
 import './App.css'
-import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
-const Dashboard = lazy(()=> import('./components/Dashboard'))
-const Landing = lazy(()=> import('./components/Landing'))
+import { useState } from 'react';
+// import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+// const Dashboard = lazy(()=> import('./components/Dashboard'))
+// const Landing = lazy(()=> import('./components/Landing'))
 
-function App() {
+// function App() {
   
-  return (
-    <div>
-      <BrowserRouter>
-      <AppBar/>
-      <Routes>
-        <Route path='/dashboard' element={<Suspense fallback={"loading..."}><Dashboard /></Suspense>}></Route>
-        <Route path='/' element={<Suspense fallback={"Loading..."}><Landing/></Suspense>}></Route>
-      </Routes>
-    </BrowserRouter>
-    </div>
+//   return (
+//     <div>
+//       <BrowserRouter>
+//       <AppBar/>
+//       <Routes>
+//         <Route path='/dashboard' element={<Suspense fallback={"loading..."}><Dashboard /></Suspense>}></Route>
+//         <Route path='/' element={<Suspense fallback={"Loading..."}><Landing/></Suspense>}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//     </div>
     
-  )
-}
+//   )
+// }
 
-function AppBar(){
-  const navigate = useNavigate();
+// function AppBar(){
+//   const navigate = useNavigate();
+//   return(
+//     <div style = {{background: "black", color: "white"}}>
+//       <button onClick={()=>{navigate("/");}} >Landing Page</button>
+//       <button onClick={()=>{ navigate("/dashboard");}} >Dashboard</button>
+//       </div>
+//   );
+// }
+function App(){
+  const [count, setCount] = useState(0);
   return(
-    <div style = {{background: "black", color: "white"}}>
-      <button onClick={()=>{navigate("/");}} >Landing Page</button>
-      <button onClick={()=>{ navigate("/dashboard");}} >Dashboard</button>
-      </div>
+    <div>
+      <Count count={count}/>
+      <Buttons />
+    </div>
   );
+}
+function Count(){
+  return <div>
+    {Count}
+  </div>
+
+}
+function Buttons(){
+  return <div>
+    <button onClick={()=>{}}>Increase</button>
+    <button onClick={()=>{}}>Decrease</button>
+  </div>
 }
 export default App
